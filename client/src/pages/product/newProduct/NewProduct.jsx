ca8bc9f81cfb822e77c2d4ad23e362a678965664
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2023-08-17 21:02:57 +0530

    new input changes

--- client/src/pages/product/newProduct/NewProduct.jsx
+++ client/src/pages/product/newProduct/NewProduct.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Formik, Form, Field} from 'formik';
 import axios from 'axios'
-import Input from '../../../components/user/register/Input';
+import {Input} from '../../../components';
 import Image from '../../../components/products/ProductImage/Image';
 import ImageUploader from '../../../components/products/ProductImage/ImageUploader'
 
@@ -13,14 +13,14 @@ const NewProduct = () => {
   
    const addProduct = (data) => {
 
-    console.log(data);
+    console.log("bye",data);
    
     axios.post("http://localhost:3001/products", data)
       .then((res) => {
         console.log(res.data);
       })
       .catch((err) => {
-        console.log(err);
+        console.log("hi",err);
       });
       setProductID(data.productID); // Set the productID state
       setProductName(data.productName); // Set the productName state
@@ -52,6 +52,7 @@ const NewProduct = () => {
       <div className="bg-white shadow-md p-8 rounded-md ">
         <h1 className="text-2xl font-bold mb-4">Product Details</h1>
       <Formik initialValues={initialValues} onSubmit={addProduct}>
+      {({errors,setFieldValue})=>(
         <Form className="w-full w-100%  flex flex-col">
           <div className="flex items-center justify-between mb-4">
             {/* Left side (fields) */}
@@ -63,20 +64,44 @@ const NewProduct = () => {
                   type="select"
                   label="Product Collection"
                   component={Input}
-            
+                  options= {[{label:"men",value:"men"}, {label:"women",value:"women"}, {label:"kids",value:"kids"}, {label:"Unisex",value:"Unisex"}]}
+                  setFieldValue={setFieldValue}
               />
-              <Field name="category" type="select" label="Category" component={Input} />
-              <Field name="brandName" type="select" label="Brand Name" component={Input} />
+              
+              <Field 
+              name="category"
+               type="select"
+                label="Category" 
+                component={Input} 
+                 options= {[{label:"Shoe",value:"Shoe"},{label:"Sneaker",value:"Sneaker"}, {label:"Flip Flops",value:"Flip Flops"},{label:"Heels",value:"Heels"}]}
+                  setFieldValue={setFieldValue}/>
+
+              <Field 
+              name="brandName"
+               type="select" 
+               label="Brand Name" 
+               component={Input}  
+               options= {[{label:"Adidas",value:"Adidas"},{label:"Nike",value:"Nike"}, {label:"Bata",value:"Bata"},{label:"DSI",value:"DSI"}]}
+               setFieldValue={setFieldValue} />
               </div>
               <div className="flex flex-col mx-4 ">
-              <Field name="sizes.size" type="select" label="Size" component={Input} />
+              <Field 
+              name="sizes.size"
+               type="select"
+                label="Size"
+                 component={Input} 
+                 options= {[{label:"30",value:"30"}]}
+                  setFieldValue={setFieldValue}
+                 /> 
               <Field
                 name="sizes.colors.color"
                 type="select"
                 label="Color"
                 component={Input}
+                options= {[{label:"blue",value:"blue"}]}
+                  setFieldValue={setFieldValue}
               />
-              <Field name="productDesc" label="Product Description" component={Input} />
+              <Field name="productDesc"  textArea label="Product Description" component={Input} />
               <Field name="regularPrice" label="Regular Price" component={Input} />
               <Field name="sellingPrice" label="Sale Price" component={Input} />
             </div>
@@ -102,6 +127,8 @@ const NewProduct = () => {
             </div>
           </div>
         </Form>
+      )}
+        
       </Formik>
     </div>
     </div>
