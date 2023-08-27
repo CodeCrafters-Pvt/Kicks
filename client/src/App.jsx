a230537c9822aa8e25cc805ae8085afc08ba3233
InhazDev <inhazfahimdev@gmail.com>
2023-08-28 01:51:10 +0530

    merge conflicts solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,26 +12,16 @@ function App() {
   const router=createBrowserRouter(
     createRoutesFromElements( 
       <Route path="/" element={<RootLayout/>}>
-        <Route  element={<AuthLayout/>}>
-            <Route path="/register" element={<RegisterUser/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/reset-password" element={<ForgotPassword/>}/>
-            <Route path="/reset-password/:token" element={<ResetPassword/>}/>
+        <Route element={<AuthLayout/>}>
+          <Route path="/register" element={<RegisterUser/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/reset-password" element={<ForgotPassword/>}/>
+          <Route path="/reset-password/:token" element={<ResetPassword/>}/>
         </Route>
         <Route element={<RequireAuth/>}>
           <Route path="/profile"  element={<Profile />} />
         </Route>    
-        <Route path="*" element={<>Missing</>} />     
-      <Route path="/" element={<Root/>}>
-          
-          <Route path="/product" element={<Products/>}/>
-          <Route path="/newproduct" element={<NewProduct/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/" element={<RegisterUser/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/ss" element={<Profile/>}/>
-          <Route path="/product" element={<Products/>}/>
-          <Route path="/newproduct" element={<NewProduct/>}/>
+        <Route path="/cart" element={<Cart/>}/>
       </Route>
     ))
   
