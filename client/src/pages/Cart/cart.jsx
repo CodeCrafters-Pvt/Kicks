2544cf0ef11f22213bd501dd0e19ebec50ee1c95
InhazDev <inhazfahimdev@gmail.com>
2023-08-28 01:42:56 +0530

    code updated

--- client/src/pages/Cart/cart.jsx
+++ client/src/pages/Cart/cart.jsx
@@ -2,76 +2,73 @@ import { useState , useEffect} from "react";
 import axios from "axios";
 import CartItems from "./cart-items";
 import { Link } from "react-router-dom";
+import { BiSolidSelectMultiple, BiChevronLeft } from "react-icons/bi"
 
 const Cart = () => {
     const [cartProducts, setCartProducts] = useState([]);
 
     useEffect(() => {
-        const fetchCartProducts = async () => {
-          try {
-            await axios.get('http://localhost:3001/cart/get-all-cart-products')
-            .then((response) => {
-              setCartProducts(response.data);
-            })
-            .catch((error) => {
-              console.log(error);
-            })
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        fetchCartProducts();
+      const fetchCartProducts = async () => {
+        try {
+          await axios.get('http://localhost:3001/cart/get-all-cart-products')
+          .then((response) => {
+            setCartProducts(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchCartProducts();
     }, []);
     
+    console.log(cartProducts);
     return (
       <>  
         <div className="container flex bg-gray-200 justify-center text-textNormal">
           <div className="w-3/4 py-4">
             <Link to="/">
-              <div className="flex items-center font-extrabold text-sm ">
-                <svg 
-                  fill="none" 
-                  stroke="currentColor" 
-                  strokeWidth={1.5} 
-                  viewBox="0 0 24 24" 
-                  xmlns="http://www.w3.org/2000/svg" 
-                  aria-hidden="true"
-                  className="w-4 h-4 mx-0"
-                >
-                  <path 
-                    strokeLinecap="round" 
-                    strokeLinejoin="round" 
-                    d="M15.75 19.5L8.25 12l7.5-7.5" 
-                  />
-                </svg>
-                <div className="ml-2">
+              <div className="flex items-center font-extrabold text-lg ">
+                <div className="text-xl">
+                  <BiChevronLeft />
+                </div>
+                <div>
                   Shopping Continue
                 </div>
               </div>
             </Link>
             {/* Horizontal line */}
-            <hr class="border-t-2 border-gray-300 my-3 w-1/3"></hr>
-            <div className="font-semibold text-xs">
+            <hr className="border-t-2 border-gray-300 my-3 w-1/3"></hr>
+            <div className="flex font-light text-lg">
               {/* shopping cart and selectall button */}
-              <div>
+              <div className="text-base">
                 Shopping cart
               </div>
-              <div>
-
-              </div>
-              {/* a text telling how many items are selected */}
-              <div>
-
+              <div className="ml-2 text-lg">
+                <BiSolidSelectMultiple />
               </div>
             </div>
-
+            <small className="text-xs font-extralight">
+                You have {cartProducts.length} item in your cart
+            </small>
             <div>
-              <div>
+              <div className="my-2 w-1/2 block">
                 {/* Block of list */}
                 <div>
                   {/* list */}
-                  <CartItems />
+                  {
+                    cartProducts.map((cartProduct) => 
+                      <CartItems 
+                        key={cartProduct._id}
+                        productName={cartProduct.productName}
+                        productType={cartProduct.category}
+                        sellingPrice={cartProduct.sellingPrice}
+                      />
+                    )
+                  }
                 </div>
               </div>
               <div>
